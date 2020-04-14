f45b36e6d6e1b5fed718c96544c138d812467db9
Misha <Mikhail.aleksandrov@nvc-inc.net>
2020-04-13 20:01:51 -0400

    implemented mongo / passport / express backend

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+const db = require("./config/keys").mongoURI;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(bodyParser.json());
+
+mongoose.connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => console.log("MongoDB connected")).catch(err => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
